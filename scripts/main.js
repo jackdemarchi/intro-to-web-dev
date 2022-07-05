fd3979262a2f446c50226733d45a8cc4161d70ef
jackdemarchi <giacomodemarchi05@gmail.com>
2022-07-05 17:11:26 +0200

    adding ball game

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Benvenuti!';
+function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result;
+  }
+
+myVariable=multiply(4,7);
