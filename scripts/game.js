5e5dd361e5f15a76c4f5febbdbf374fd11e5ba35
jackdemarchi <giacomodemarchi05@gmail.com>
2022-07-08 17:37:12 +0200

    new pages

---
+++ scripts/game.js
@@ -0,0 +1,79 @@
+function ballGame() {
+  const ball = document.getElementById("ball");
+
+  function up() {
+    // const top = parseFloat(ball.style.top);
+    // ball.style.top = top - 10 + "px";
+    BALL_VELOCITY = Math.min(3, BALL_VELOCITY + 0.1);
+  }
+  function down() {
+    // const top = parseFloat(ball.style.top);
+    // ball.style.top = top + 10 + "px";
+    BALL_VELOCITY = Math.max(0, BALL_VELOCITY - 0.1);
+  }
+  function left() {
+    const right = parseFloat(ball.style.right);
+    ball.style.right = right + 10 + "px";
+    console.log("ciuppa");
+  }
+  function right() {
+    const right = parseFloat(ball.style.right);
+    ball.style.right = right - 10 + "px";
+  }
+
+  document.onkeydown = function (event) {
+    switch (event.keyCode) {
+      case 37:
+        left();
+        console.log("Left key is pressed.");
+        break;
+      case 38:
+        up();
+        break;
+      case 39:
+        right();
+        console.log("Right key is pressed.");
+        break;
+      case 40:
+        down();
+        break;
+    }
+  };
+
+  let start = undefined;
+  let previousTimeStamp = 0;
+  let done = false;
+  let BALL_VELOCITY = 0.5;
+  let ballX = 0;
+  let ballDirectionX = 1;
+
+  function step(timestamp) {
+    if (start === undefined) {
+      start = timestamp;
+    }
+    const elapsed = timestamp - start;
+    const deltaTime = timestamp - previousTimeStamp;
+
+    if (previousTimeStamp !== timestamp) {
+      // Math.min() is used here to make sure the element stops at exactly 200px
+      const deltaDistance = deltaTime * BALL_VELOCITY;
+      const ballWidth = ball.getBoundingClientRect().width;
+      if (
+        ballDirectionX === 1 &&
+        ballX + deltaDistance >= window.innerWidth - ballWidth
+      ) {
+        ballDirectionX = -1;
+      } else if (ballDirectionX === -1 && ballX - deltaDistance <= 0) {
+        ballDirectionX = 1;
+      }
+      ballX += deltaDistance * ballDirectionX;
+      ball.style.transform = "translateX(" + ballX + "px)";
+    }
+    previousTimeStamp = timestamp;
+    window.requestAnimationFrame(step);
+  }
+
+  window.requestAnimationFrame(step);
+}
+
+ballGame();
--- scripts/main.js
+++ scripts/main.js
@@ -2,81 +2,34 @@ const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Benvenuti!';
 
 
-function ballGame() {
-  console.log("ballGame start");
-  const ball = document.getElementById("ball");
-
-  function up() {
-      const top = parseFloat(ball.style.top);
-      ball.style.top = (top - 10) + "px";
-  }
-  function down() {
-      const top = parseFloat(ball.style.top);
-      ball.style.top = (top + 10) + "px";
-     }
-  function left() {
-      const right = parseFloat(ball.style.right);
-      ball.style.right = (right + 10) + "px";
-      console.log("ciuppa");
-  }
-  function right() {
-      const right = parseFloat(ball.style.right);
-      ball.style.right = (right - 10) + "px";
-  }
-
-  document.onkeydown = function (event) {
-      switch (event.keyCode) {
-          case 37:
-              left();
-              console.log("Left key is pressed.");
-              break;
-          case 38:
-              up();
-              break;
-          case 39:
-              right();
-              console.log("Right key is pressed.");
-              break;
-          case 40:
-              down();
-              break;
-      }
-  };
-};
+function sum(){
+    const firstOperand = getOperand("first-operand");
+    const secondOperand = getOperand("second-operand");
+    setResult(firstOperand + secondOperand);
+}
+function diff(){
+    const firstOperand = getOperand("first-operand");
+    const secondOperand = getOperand("second-operand");
+    setResult(firstOperand - secondOperand);
+}
+function divide(){
+    const firstOperand = getOperand("first-operand");
+    const secondOperand = getOperand("second-operand");
+    setResult(firstOperand / secondOperand);
+}
+function multiply(){
+    const firstOperand = getOperand("first-operand");
+    const secondOperand = getOperand("second-operand");
+    setResult(firstOperand * secondOperand);
+}
 
 
+function getOperand(operandId){
+    const operandTextValue = document.getElementById(operandId).value;
+    const operand = parseFloat(operandTextValue);
+    return operand;
+}
 
-  function sum(){
-      const firstOperand = getOperand("first-operand");
-      const secondOperand = getOperand("second-operand");
-      setResult(firstOperand + secondOperand);
-  }
-  function diff(){
-      const firstOperand = getOperand("first-operand");
-      const secondOperand = getOperand("second-operand");
-      setResult(firstOperand - secondOperand);
-  }
-  function divide(){
-      const firstOperand = getOperand("first-operand");
-      const secondOperand = getOperand("second-operand");
-      setResult(firstOperand / secondOperand);
-  }
-  function multiply(){
-      const firstOperand = getOperand("first-operand");
-      const secondOperand = getOperand("second-operand");
-      setResult(firstOperand * secondOperand);
-  }
-
-
-  function getOperand(operandId){
-      const operandTextValue = document.getElementById(operandId).value;
-      const operand = parseFloat(operandTextValue);
-      return operand;
-  }
-
-  function setResult(result) {
-      document.getElementById("result").innerText = result;
-  }
-  
-
-ballGame();
+function setResult(result) {
+    document.getElementById("result").innerText = result;
+}
