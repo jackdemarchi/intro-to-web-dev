c6ca30d7270817efc5fd9196905eedca6745a5cc
jackdemarchi <giacomodemarchi05@gmail.com>
2022-07-13 15:07:07 +0200

    adding story-generator

--- scripts/main3.js
+++ scripts/main3.js
@@ -1,22 +1,22 @@
 function main3() {
-    let myButton = document.querySelector('#change-user');
-    let myHeading = document.querySelector('h1');
-    function setUserName(){
-        let myName = prompt('please enter your name.');
-        localStorage.setItem('name', myName);
-        myHeading.textContent = ' This site is cool, '+ myName + ' isn\'t it?';
-    }
-    if(!localStorage.getItem('name')) {
-        setUserName();
-    } else {
-        let storedName = localStorage.getItem('name');
-        myHeading.textContent = 'This site is cool, ' + storedName + ' isn\'t it?';
-    }
-    myButton.onclick = function(event) {
-        const value = event.target.textContent;
-        console.log("clicked", value);
-        setUserName();
-    }    
+  let myButton = document.querySelector("#change-user");
+  let myHeading = document.querySelector("h1");
+  function setUserName() {
+    let myName = prompt("please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = " This site is cool " + myName + ", isn't it?";
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "This site is cool " + storedName + ", isn't it?";
+  }
+  myButton.onclick = function (event) {
+    const value = event.target.textContent;
+    console.log("clicked", value);
+    setUserName();
+  };
 }
 
 main3();
